fee90de09d3154af663951e983e26e10d5ca09ff
damda0904 <suen0904@gmail.com>
2022-01-18 19:53:09 +0900

    2022.01.18

--- Jan 4th/src/TwoArray/TwoArray.java
+++ Jan 4th/src/TwoArray/TwoArray.java
@@ -1,5 +1,12 @@
 package TwoArray;
 
+/*
+    2022.01.18
+    백준 2143번 "두 배열의 합"
+
+    Long과 long은 다르다. 진짜 짜증나
+ */
+
 import java.io.*;
 import java.util.*;
 
@@ -13,32 +20,32 @@ public class TwoArray {
         long T = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
 
-        Long[] first = new Long[n+1];
+        long[] first = new long[n+1];
         StringTokenizer st1 = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             first[i] = Long.parseLong(st1.nextToken());
         }
 
         int m = Integer.parseInt(br.readLine());
-        Long[] second = new Long[m+1];
+        long[] second = new long[m+1];
         StringTokenizer st2 = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++) {
             second[i] = Long.parseLong(st2.nextToken());
         }
 
-        List<Long> onesub = makeSub(first, n);
-        List<Long> twosub = makeSub(second, m);
+        long[] onesub = makeSub(first, n);
+        long[] twosub = makeSub(second, m);
 
         find(onesub, twosub, T);
 
 
     }
 
-    static List<Long> makeSub(Long[] ori, int size) {
+    static long[] makeSub(long[] ori, int size) {
         List<Long> sub = new ArrayList<>();
 
         for(int i = 0; i < size; i++) {
-            Long sum = ori[i];
+            long sum = ori[i];
             sub.add(sum);
             for(int j = i + 1; j < size; j++) {
                 sum += ori[j];
@@ -48,35 +55,37 @@ public class TwoArray {
 
         //정렬
         Collections.sort(sub);
+        long[] sub_array = sub.stream().mapToLong(Long::longValue).toArray();
 
-        return sub;
+        return sub_array;
     }
 
-    static void find(List<Long> fst, List<Long> sec, Long T) {
+    static void find(long[] fst, long[] sec, long T) {
 
         Long answer = Long.valueOf(0);
-        int pt1 = 0; int pt2 = sec.size() - 1;
-        Long count1 =  Long.valueOf(0); Long count2 =  Long.valueOf(0);
+        int pt1 = 0; int pt2 = sec.length - 1;
+
         long sum = 0;
 
-        while(pt1 < fst.size() && pt2 > -1) {
-            sum = fst.get(pt1) + sec.get(pt2);
+        while(pt1 < fst.length && pt2 > -1) {
+            sum = fst[pt1] + sec[pt2];
 
             //sum == T
             if(sum == T) {
                 //동률 체크
-                Long check = fst.get(pt1);
-                while(pt1 < fst.size() && check == fst.get(pt1)) {
+                long count1 = 0; long count2 = 0;
+                long check = fst[pt1];
+                while(pt1 < fst.length && fst[pt1] == check) {
                     count1++;
                     pt1++;
                 }
-                check = sec.get(pt2);
-                while(pt2 > -1 && check == sec.get(pt2)){
+                check = sec[pt2];
+                while(pt2 > -1 && sec[pt2] == check){
                     count2++;
                     pt2--;
                 }
 
-                answer += (Long)(count1 * count2);
+                answer += (long)(count1 * count2);
             }
 
             //sum > T
